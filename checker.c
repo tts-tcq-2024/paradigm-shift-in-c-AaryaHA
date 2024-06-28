e2e65df498eafb0a82b906eb6187b390107d9b93
AaryaHA <haprasad2000@gmail.com>
2024-06-28 16:25:58 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,21 +1,63 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define TEMPERATURE_MIN 0
+#define TEMPERATURE_MAX 45
+#define SOC_MIN 20
+#define SOC_MAX 80
+#define CHARGE_RATE_MAX 0.8
+
+typedef enum {
+    OK,
+    TEMPERATURE_OUT_OF_RANGE,
+    SOC_OUT_OF_RANGE,
+    CHARGE_RATE_OUT_OF_RANGE
+} BatteryStatus;
+
+BatteryStatus checkTemperature(float temperature) {
+    if (temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX) {
+        return TEMPERATURE_OUT_OF_RANGE;
+    }
+    return OK;
+}
+
+BatteryStatus checkSoc(float soc) {
+    if (soc < SOC_MIN || soc > SOC_MAX) {
+        return SOC_OUT_OF_RANGE;
+    }
+    return OK;
+}
+
+BatteryStatus checkChargeRate(float chargeRate) {
+    if (chargeRate > CHARGE_RATE_MAX) {
+        return CHARGE_RATE_OUT_OF_RANGE;
+    }
+    return OK;
+}
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+    BatteryStatus tempStatus = checkTemperature(temperature);
+    if (tempStatus != OK) {
+        printf("Temperature out of range!\n");
+        return 0;
+    }
+
+    BatteryStatus socStatus = checkSoc(soc);
+    if (socStatus != OK) {
+        printf("State of Charge out of range!\n");
+        return 0;
+    }
+
+    BatteryStatus chargeRateStatus = checkChargeRate(chargeRate);
+    if (chargeRateStatus != OK) {
+        printf("Charge Rate out of range!\n");
+        return 0;
+    }
+
+    return 1;
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
 }
