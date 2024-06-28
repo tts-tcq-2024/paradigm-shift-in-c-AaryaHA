02d6437bcbd261933b9a64f6eb1a39cbc3e12507
AaryaHA <haprasad2000@gmail.com>
2024-06-28 17:56:44 +0530

    Update checker.c

--- .github/workflows/checker.c
+++ .github/workflows/checker.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
 #include <assert.h>
-
-#define TEMPERATURE_MIN 0
-#define TEMPERATURE_MAX 45
-#define SOC_MIN 20
-#define SOC_MAX 80
-#define CHARGE_RATE_MAX 0.8
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    int ok = 1; // Assume everything is OK unless proven otherwise
-
-    // Check temperature and state of charge together
-    if (temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX ||
-        soc < SOC_MIN || soc > SOC_MAX) {
-        if (temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX) {
-            printf("Temperature out of range!\n");
-        }
-        if (soc < SOC_MIN || soc > SOC_MAX) {
-            printf("State of Charge out of range!\n");
-        }
-        ok = 0;
-    }
-
-    // Check charge rate
-    if (chargeRate > CHARGE_RATE_MAX) {
-        printf("Charge Rate out of range!\n");
-        ok = 0;
-    }
-
-    return ok;
+ 
+int isTemperatureOk(float temperature) {
+  if (temperature < 0 || temperature > 45) {
+    printf("Temperature out of range!\n");
+    return 0;
+  }
+  return 1;
 }
-
+ 
+int isSocOk(float soc) {
+  if (soc < 20 || soc > 80) {
+    printf("State of Charge out of range!\n");
+    return 0;
+  }
+  return 1;
+}
+ 
+int isChargeRateOk(float chargeRate) {
+  if (chargeRate > 0.8) {
+    printf("Charge Rate out of range!\n");
+    return 0;
+  }
+  return 1;
+}
+ 
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
+}
+ 
 int main() {
-    assert(batteryIsOk(25, 70, 0.7));
-    assert(!batteryIsOk(50, 85, 0));
+  assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
 }
