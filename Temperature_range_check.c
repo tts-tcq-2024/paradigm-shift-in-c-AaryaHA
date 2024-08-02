98361997c58426afd76adf80f18796ffd6e390c3
AaryaHA <haprasad2000@gmail.com>
2024-08-02 17:39:53 +0530

    Create Temperature_range_check.c

---
+++ Temperature_range_check.c
@@ -0,0 +1,8 @@
+#include "Temperature_rang_check.h"
+#include "value_range_check.h"
+#include "warning.h"
+
+int isTemperatureOk(float temperature) {
+    checkEarlyWarning(temperature, 0, 45, 2.25, "Warning: Approaching lower temperature limit", "Warning: Approaching upper temperature limit");
+    return isValueInRange(temperature, 0, 45, "Temperature out of range!");
+}
