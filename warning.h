ee4eb687108360bb6519b66ced8b19845daf48f6
AaryaHA <haprasad2000@gmail.com>
2024-08-02 17:36:51 +0530

    Update warning.h

--- warning.h
+++ warning.h
@@ -1,10 +1,9 @@
 #ifndef EARLYWARNING_H
 #define EARLYWARNING_H
-#ifndef WARNINGMESSAGE_PRINTER_H
-#define WARNINGMESSAGE_PRINTER_H
+
 
 void checkEarlyWarning(float value, float min, float max, float tolerance, const char* lowWarning, const char* highWarning);
 void checkEarlyWarningForChargeRate(float value, float max, float tolerance, const char* highWarning);
 
-void printWarningMessage(const char* WarningMessage);
+
 #endif 
